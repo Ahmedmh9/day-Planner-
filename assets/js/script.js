2f5fcb31ba20df6a596b62b0663eaa033cafa70f
Mohamed Ahmed <axmedmohamed7@gmail.com>
2021-07-21 22:29:06 -0500

    working

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,9 @@ var curentTime= document.querySelector('#currentDay');
 console.log(curentTime);
 document.getElementById("currentDay").innerHTML = time;
 
-let getTextarea = function (hour) {
-   let schedule = JSON.parse(window.localStorage.getItem("discribtion"));
-
-   if (!discribtion) {
-       discribtion = [];
-   } else {
-      for (let i =0; i< discribtion.length; i++) {
-          if (discribtion[i].hour == hour) {
-              return discribtion[i].text;
-           }
-      }
-   }
-   return "";
-}
-  console.log(localStorage)
-
-
-//const button = document.querySelector('.saveBtn');
+const button = document.querySelector('.saveBtn');
 
 //create a varible that saves whatever they put in the discribtion colum into the local storege.
-
-
 //add event listner to the save button and save it to the 
 //button.addEventListener('click' );
 
